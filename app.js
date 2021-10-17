c4a697eb1242db8ee8eb102a83b63e5c5faabaa7
vakyfunky <cve.vanja@gmail.com>
2021-10-17 17:59:10 +0200

    master

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ const randomFunc = {
 };
 
 // *****
-// getting values
-
+// getting values - event listeners
 generateEl.addEventListener('click', () => {
     const length = +lengthEl.value;
     const hasUpper = upperEl.checked;
@@ -29,23 +28,55 @@ generateEl.addEventListener('click', () => {
     resultEl.innerText = generatePassword(length, hasLower, hasUpper, hasNumbers, hasSymbols);
 })
 
+// Copy password to clipboard
+clipboardEl.addEventListener('click', () => {
+    const textarea = document.createElement('textarea');
+    const password = resultEl.innerText;
+
+    if (!password) {
+        return;
+    }
+
+    textarea.value = password;
+
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+    alert('Password copied to clipboard');
+})
+
 // *****
 // Generate Pass function
-function generatePassword(length, lower, upper, number, symbol){
-// 1. Init passwoord variable
-// 2. Filter out unchecked types
-// 3. Loop over the length, call generator function for each type
-// 4. Add the final password to the password variable and return
+function generatePassword(length, lower, upper, number, symbol) {
+    // 1. Init passwoord variable
+    // 2. Filter out unchecked types
+    // 3. Loop over the length, call generator function for each type
+    // 4. Add the final password to the password variable and return
 
-let generatedPassword = '';
+    let generatedPassword = '';
 
-// count checked options
-const typesCount = lower + upper + number + symbol;
-// array based on typesCount
-const typesArray = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+    // count checked options
+    const typesCount = lower + upper + number + symbol;
+    // array based on typesCount
+    const typesArray = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0]);
 
-console.log(typesArray);
+    console.log(typesArray);
 
+    if (typesCount === 0) {
+        return '';
+    }
+
+    for (let i = 0; i < length; i += typesCount) {
+        typesArray.forEach(type => {
+            const funcName = Object.keys(type)[0];
+
+            generatedPassword += randomFunc[funcName]();
+        })
+    }
+    const finalPass = generatedPassword.slice(0, length);
+    console.log(finalPass);
+    return finalPass;
 }
 
 // *****
